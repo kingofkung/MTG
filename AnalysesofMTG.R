f02cf4b052d66f769d96bf9bbb917307d28cb99c
Ben Rogers <bjroger@ku.edu>
2016-04-16 10:47:13 -0500

    added some notes!

--- AnalysesofMTG.R
+++ AnalysesofMTG.R
@@ -18,5 +18,12 @@ t.test( x = mtg[grep("goblin", mtg$subtypes, ignore.case = T), "numpow"],
 t.test( x = mtg[grep("wall", mtg$subtypes, ignore.case = T), "numtough"],
        y =  mtg[grep("beast", mtg$subtypes, ignore.case = T), "numtough"])
 
-t.test(x = mtg[grep("wizard", mtg$type, ignore.case = T), "cmc"],
-       y = mtg[grep("beast", mtg$type, ignore.case = T), "cmc"])
+t.test(x = mtg[grep("wizard", mtg$subtypes, ignore.case = T), "cmc"],
+       y = mtg[grep("beast", mtg$subtypes, ignore.case = T), "cmc"])
+
+t.test(x = mtg[mtg$Blue ==1 & mtg$types %in% "Creature", "cmc"],
+       y = mtg[mtg$Red == 1 & mtg$types %in% "Creature", "cmc"]
+       )
+
+## Note to self: {U} is actually blue in manaCost
+## Also, it's types and subtypes, not type or subtype

e11ef4af6040689aec5e61c86042358721769431
Ben Rogers <bjroger@ku.edu>
2016-04-24 13:39:05 -0500

    Eliminate comments that aren't relevant any more

--- AnalyzeMTG.R
+++ AnalyzeMTG.R
@@ -72,7 +72,4 @@ source(paste0(loc, "/Printings.R"))
 
 source(paste0(loc, "/CardAbilities.R"))
 
-## Given a set of variables with all printings, how can I tell R to include a 1 if the card has a value in this printing, and 0 if it does not?
-
-## While the thing I have above works, it would be better if it were all part of 1 loop
 write.csv(df, paste0(writeloc,"MTGData.csv"), row.names = FALSE)

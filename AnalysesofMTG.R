b7e7d2327be9c0e056435b0aa72966a5078e006e
Ben Rogers <bjroger@ku.edu>
2016-04-23 23:45:41 -0500

    made it so rare goblins features all columns of data

--- AnalysesofMTG.R
+++ AnalysesofMTG.R
@@ -1,7 +1,7 @@
 ## Here's where I'll keep all of my analyses, graphical and statistical
 
 ## This looks pretty decent...
-ifelse(exists("df"), mtg <- df, mtg <- read.csv("/Users/bjr/Dropbox/MTGData.csv")
+if(exists("df")) mtg <- df else mtg <- read.csv("/Users/bjr/Dropbox/MTGData.csv")
 mtg$numpow <- as.numeric(as.character(mtg$power))
 ## with(mtg, table(power, numpow))
 mtg$numtough <- as.numeric(as.character(mtg$toughness))
@@ -14,13 +14,12 @@ as.character(unique(mtg$modalrarity))
 
 ## Quick note: if you want to use subtypes like this, it has to be capitalized
 ## Or you can use grepl to get the true false while ignoring case
-raregoblins <- mtg[grepl("Goblin", mtg$subtypes, ignore.case = TRUE) & mtg$modalrarity == "Rare" ,
-                   c("name", "type", "subtypes", "power", "toughness", "modalrarity")]
+raregoblins <- mtg[grepl("Goblin", mtg$subtypes, ignore.case = TRUE) & mtg$modalrarity == "Rare" ,]
 
 poisons <- mtg[grepl("poison", mtg$text, ignore.case = TRUE), ]
 
 ## write.csv(file = "/Users/bjr/Dropbox/raregoblins.csv", raregoblins)
-write.csv(file = '/Users/bjr/Dropbox/poisonrefs.csv', poisons, row.names = F)
+## write.csv(file = '/Users/bjr/Dropbox/poisonrefs.csv', poisons, row.names = F)
 
 nrow(mtg[mtg$subtypes %in% "Goblin",])
 
